59e0cd85af24ea25aa45ea72eeea65277346746c
ahmaziz12 <ahm.aziz12@gmail.com>
2021-10-28 17:24:26 +0500

    Update page edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, length: { maximum: 30}
+  validates :username, length: { minimum:1, maximum: 30}
   validates :phone, numericality: true, length: { minimum: 10, maximum: 15 }, uniqueness: true, allow_blank: true
   validates_format_of :password, with: /^(?=.*?[A-Z])(?=.*?[#?!@$%^&*-]).{8,}$/, multiline: true, message: "(minimum 8 characters with at least one capital letter and a special character)"
   validates :email,  presence: true, if: -> { phone.blank? }
