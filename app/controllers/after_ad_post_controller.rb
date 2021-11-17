b4ffd29e8b0be4d4711f4bdae2dd38f1a52dd783
ahmaziz12 <ahm.aziz12@gmail.com>
2021-11-17 17:06:50 +0500

    update action changed

--- app/controllers/after_ad_post_controller.rb
+++ app/controllers/after_ad_post_controller.rb
@@ -12,23 +12,21 @@ class AfterAdPostController < ApplicationController
     @ad = Ad.find(params[:ad_id])
     case step
       when :second_step
-        if ((params[:ad]).present?)
-          if ((params[:ad][:images].count + @ad.images.count) <= 5)
-            @ad.images.attach(params[:ad][:images])
-            render_wizard(@ad,{}, ad_id: @ad)
-          else
-            redirect_to after_ad_post_path(:second_step, ad_id: @ad), alert: "maximum 5 images can be attatched"
+        if params.dig(:ad, :images).present?
+          @ad.images.attach(params[:ad][:images])
+          if @ad.errors[:images].present?
+            @ad.reload
+            byebug
+            redirect_to after_ad_post_path(:second_step, ad_id: @ad), alert: @ad.errors.messages[:images].first
+            return
           end
-        else
-          render_wizard(@ad,{}, ad_id: @ad)
         end
       when :third_step
         if @ad.update_attribute(:secondary_contact, params[:secondary_contact])
-          redirect_to @ad
-        else
-          render_wizard(@ad,{}, ad_id: @ad)
+          redirect_to @ad and return
         end
       end
+    render_wizard(@ad, {}, ad_id: @ad)
   end
 
   def destroy
